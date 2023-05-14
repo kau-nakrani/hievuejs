90c650aa59d13eaed81bdc0a4ce1dce484073e05
Viram Goyani <vaghasiyab347@gmail.com>
2023-05-13 22:52:07 -0700

    working on

---
+++ src/views/SHHome/SHHero/SHSatisfiedCustomer.vue
@@ -0,0 +1,36 @@
+
+<script setup>
+    // import IconDownloadWhite from '../../../components/icons/IconDownloadWhite.vue';
+    // import IconPro from '../../../components/icons/IconPro.vue';
+</script>
+
+<template>
+    <section className="satisfied-customer-section img-cover section-extra-big-py">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2 className="heading-title small-heading-title text-center">Satisfied our customers</h2>
+                    <p className="label-heading-title">Heard about us? Listen to what our customer has to say about us.</p>
+                    <div class="satisfied-customer-box">
+                        <div class="satisfied-customer-img-box"></div>
+                        <h5>Goliathks</h5>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                        <p>"I was looking for a new plugin to not show products from certain categories on the shop page.It seems to work well at first sight. I will definitely keep testing it, thanks to the developers."</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> 
+</template>
+<style>
+.satisfied-customer-section{
+    background: url(./../../../assets/images/banner/satisfied-bg.png) var(--light-white-color);
+    width: 100%;
+    min-height: 800px;
+}
+</style>
--- src/views/SHHome/SHHome.vue
+++ src/views/SHHome/SHHome.vue
@@ -3,10 +3,12 @@
     import SHFaq from "./SHHero/SHFaq.vue";
     import SHHome from "./SHHero/SHHero.vue";
     import SHWhyChoose from "./SHHero/SHWhyChoose.vue";
+    import SHSatisfiedCustomer from "./SHHero/SHSatisfiedCustomer.vue";
 </script>
 <template>
     <SHHome />
     <SHExcellentFeatures/>
     <SHWhyChoose/>
+    <SHSatisfiedCustomer/>
     <SHFaq/>
 </template>
